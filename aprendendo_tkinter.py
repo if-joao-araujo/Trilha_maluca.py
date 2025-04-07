5664930dd99e89df59cb868d3ed1f683d470ac90
João pedro santana de araújo <if.joao.araujo@gmail.com>
2025-04-07 10:19:55 -0300

    aprendendo_tkinter.py
    estou aprendendo a usar o tkinter para adicionar interface no jogo

---
+++ aprendendo_tkinter.py
@@ -0,0 +1,18 @@
+
+from tkinter import *
+from tkinter import ttk
+
+janela = Tk()
+frame1 = Frame(janela)
+frame1.pack()
+
+butao = Button(frame1, text="Sim")
+butao["width"] = 50
+butao.pack(side="left")
+butao.bind("")
+
+butao2 = butao = Button(frame1, text="Não")
+butao2["width"] = 50
+butao2.pack(side="right")
+janela.mainloop()
+
